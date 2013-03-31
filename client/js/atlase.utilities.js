a690eab3c846d70bf281b6130881b2f90417bd25
Sergei Petrunin <halfstrik@gmail.com>
2013-04-01 02:48:22 +0400

    Minor refactoring: removed unnecessary (at least for now) map, used simple object instead.

--- client/js/atlase.utilities.js
+++ client/js/atlase.utilities.js
@@ -1,19 +1,17 @@
 /*global ATLASE_UTILITIES*/
 var ATLASE_UTILITIES = ATLASE_UTILITIES || (function () {
     "use strict";
-    var worldDataParts = {};
+    var worldData;
 
     return {
         loadWorldStaticData: function (worldJsonFileName, callback) {
             var jsonRequest,
-                restArguments,
-                worldDataPart;
-            if (worldDataParts.hasOwnProperty(worldJsonFileName)) {
+                restArguments;
+            if (worldData !== undefined) {
                 restArguments = Array.prototype.slice.call(arguments, 2);
                 callback.apply(null, restArguments);
             }
-            worldDataPart = {};
-            worldDataParts[worldJsonFileName] = worldDataPart;
+            worldData = {};
             jsonRequest = new XMLHttpRequest();
             jsonRequest.open('GET', worldJsonFileName, true);
             jsonRequest.onload = function () {
@@ -32,7 +30,7 @@ var ATLASE_UTILITIES = ATLASE_UTILITIES || (function () {
                             tile.imagewidth = imagewidth;
                             tile.tileheight = tileheight;
                             tile.tilewidth = tilewidth;
-                            worldDataPart.tileset.push(tile);
+                            worldData.tileset.push(tile);
                             loadedNumberOfImages += 1;
                             if (loadedNumberOfImages === totalNumberOfImages) {
                                 restArguments = Array.prototype.slice.call(arguments, 2);
@@ -41,14 +39,14 @@ var ATLASE_UTILITIES = ATLASE_UTILITIES || (function () {
                         };
                         image.src = imagePath;
                     };
-                worldDataPart.height = parsedJson.height;
-                worldDataPart.width = parsedJson.width;
-                worldDataPart.tileheight = parsedJson.tileheight;
-                worldDataPart.tilewidth = parsedJson.tilewidth;
-                worldDataPart.layers = parsedJson.layers;
+                worldData.height = parsedJson.height;
+                worldData.width = parsedJson.width;
+                worldData.tileheight = parsedJson.tileheight;
+                worldData.tilewidth = parsedJson.tilewidth;
+                worldData.layers = parsedJson.layers;
                 totalNumberOfImages = parsedJson.tilesets.length;
                 loadedNumberOfImages = 0;
-                worldDataPart.tileset = [];
+                worldData.tileset = [];
                 for (i = 0; i < totalNumberOfImages; i += 1) {
                     rawTile = parsedJson.tilesets[i];
                     loadImage(rawTile.image, rawTile.firstgid, rawTile.imageheight, rawTile.imagewidth,
